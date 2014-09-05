988a0ac6103c06b4bfa34368e864e11f479cd499
jrabbit <jackjrabbit+github@gmail.com>
2014-09-04 22:42:41 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+setup(name="taskc",
+      version="0.0.1a1",
+      packages=["taskc"],
+      author="Jack Laxson",
+      author_email="jackjrabbit@gmail.com",
+      description="",
+      license="GPL v3+",
+      install_requires=["taskw"],
+      url="https://github.com/jrabbit/taskd-client-py",
+      classifiers=["License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)", 
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Developers",
+                  ])
