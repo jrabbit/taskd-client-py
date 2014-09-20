bfd077e2d768e337670065087a5c12651e68defd
jrabbit <jackjrabbit+github@gmail.com>
2014-09-20 19:48:40 -0400

    some progress.. details are for a demo instance of taskd

--- taskc/connect.py
+++ taskc/connect.py
@@ -1,17 +1,42 @@
-import ssl
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+
+import transaction
+
+
 # - Server address and port - TaskdConnection call
-# - Organization name - 
+# - Organization name -
 # - User name
 # - Password [secret]
 # - Certificate [secret]
 # - Key [secret]
-class TaskdConnection(object):
 
-    def __init__(self, host, port=53589, **kwargs):
-        pass
-    def connect(self):
-        # do we need this?
-        # self.cadata = getcadata()
-        self.context = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH)
-        self.connection =  context.wrap_socket(socket.socket(socket.AF_INET))
-        self.connection.connect(self.host, self.port)
+# class TaskdConnection(object):
+
+#     def __init__(self, host, port=53589, **kwargs):
+#         pass
+#     def connect(self):
+# do we need this?
+# self.cadata = getcadata()
+#         self.context = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH)
+#         self.connection =  context.wrap_socket(socket.socket(socket.AF_INET))
+#         self.connection.connect(self.host, self.port)
+
+
+
+class TaskClient(Protocol):
+    def sendMessage(self, msg):
+        m = transaction.mk_message(org="public",user="jack",key="a69f8b68-5747-4ed3-af70-e891e9888fff")
+        self.transport.write("Hello")
+
+
+def got_protocol(p):
+    p.sendMessage("Hello")
+    reactor.callLater(1, p.sendMessage, "This is sent in a second")
+    reactor.callLater(2, p.transport.loseConnection)
+
+point = TCP4ClientEndpoint(reactor, "localhost", 53589)
+d = connectProtocol(point, TaskClient())
+d.addCallback(got_protocol)
+reactor.run()
--- taskc/transaction.py
+++ taskc/transaction.py
@@ -1,3 +1,14 @@
+from email.message import Message
+
+def mk_message(org, user, key):
+    m = Message()
+    m['client'] = "taskc-py 0.0.1a1"
+    m['protocol'] = "v1"
+    m['org'] = org
+    m['user'] = user
+    m['key'] = key
+    return m
+
 class Transaction(object):
     """ Each transaction is a single incoming message, with a single response
    message.  All communication therefore consists of a single 'send', followed
