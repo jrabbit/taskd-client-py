ef4aa2090da5c0ccf4dcb443d44ceffe21c0eadb
jrabbit <jackjrabbit+github@gmail.com>
2015-01-29 15:22:41 -0500

    refactoring for general use

--- taskc/simple.py
+++ taskc/simple.py
@@ -2,20 +2,41 @@ import ssl
 import socket
 import struct
 import email
+import os.path
 
 import transaction
 import errors
 
-def connect():
+def connect(client_cert, client_key, server, port=53589, cacert=None):
     c = ssl.create_default_context()
-    c.load_cert_chain("/home/jack/.task/jacklaxson.cert.pem", "/home/jack/.task/jacklaxson.key.pem")
-    c.load_verify_locations("/home/jack/.task/ca.cert.pem")
+    print client_cert, client_key, server, port, cacert
+    c.load_cert_chain(client_cert, client_key)
+    if cacert:
+        c.load_verify_locations(cacert)
     c.check_hostname = False
     conn = c.wrap_socket(socket.socket(socket.AF_INET))
-    conn.connect(("192.168.1.110", 53589))
+    conn.connect((server, port))
     return conn
+
+def from_taskrc():
+    conf = dict([x.replace("\\/", "/").strip().split('=') for x in open(
+        os.path.expanduser("~/.taskrc")).readlines() if '=' in x and x[0] != "#"])
+    #TODO parse out credentials
+    return connect(client_cert=conf['taskd.certificate'],
+        client_key=conf['taskd.key'],
+        server=conf['taskd.server'].split(":")[0],
+        port=int(conf['taskd.server'].split(":")[1]),
+        cacert=conf['taskd.ca'] if 'taskd.ca' in conf else None,
+        )
+def manual():
+    # Task 2.3.0 doesn't let you have a cacert if you enable trust
+    return connect(client_cert="/home/jack/.task/jacklaxson.cert.pem",
+        client_key="/home/jack/.task/jacklaxson.key.pem",
+        cacert="/home/jack/.task/ca.cert.pem",
+        server="192.168.1.110",
+        )
 # print conn.getpeercert()
-connect()
+conn = manual()
 msg = transaction.mk_message("Public", "Jack Laxson", "f60bfcb9-b7b8-4466-b4c1-7276b8afe609")
 msg['type'] = "statistics"
 our_len = long(len(msg.as_string()) + 4)
