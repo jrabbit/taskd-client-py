ed24f9f75dbec330c58e3b1d6958c778d9ac36b5
jrabbit <jackjrabbit+github@gmail.com>
2015-01-29 13:57:40 -0500

    we can connect!

--- taskc/simple.py
+++ taskc/simple.py
@@ -1,5 +1,8 @@
 import ssl
 import socket
+import struct
+
+import transaction
 
 c = ssl.create_default_context()
 c.load_cert_chain("/home/jack/.task/jacklaxson.cert.pem", "/home/jack/.task/jacklaxson.key.pem")
@@ -8,3 +11,10 @@ c.check_hostname = False
 conn = c.wrap_socket(socket.socket(socket.AF_INET))
 conn.connect(("192.168.1.110", 53589))
 print conn.getpeercert()
+msg = transaction.mk_message("Public", "Jack Laxson", "f60bfcb9-b7b8-4466-b4c1-7276b8afe609")
+msg['type'] = "statistics"
+our_len = long(len(msg.as_string()) + 4)
+size = struct.pack('>L', our_len)
+# print size
+conn.sendall(size+msg.as_string())
+print conn.recv(4096)
--- taskc/transaction.py
+++ taskc/transaction.py
@@ -11,6 +11,6 @@ def mk_message(org, user, key):
 
 class Transaction(object):
     """ Each transaction is a single incoming message, with a single response
-   message.  All communication therefore consists of a single 'send', followed
-   by a single 'receive', then termination."""
-   pass
+    message.  All communication therefore consists of a single 'send', followed
+    by a single 'receive', then termination."""
+    pass
