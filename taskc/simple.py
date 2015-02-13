c6419341a4c55443ea66d011864b7219a7d841fe
jrabbit <jackjrabbit+github@gmail.com>
2015-02-13 15:07:24 -0500

    refactor simple.py, move some of the logic over to  transaction.py

--- taskc/simple.py
+++ taskc/simple.py
@@ -7,28 +7,51 @@ import os.path
 import transaction
 import errors
 
-def connect(client_cert, client_key, server, port=53589, cacert=None):
-    c = ssl.create_default_context()
-    print client_cert, client_key, server, port, cacert
-    c.load_cert_chain(client_cert, client_key)
-    if cacert:
-        c.load_verify_locations(cacert)
-    #enable for non-selfsigned certs
-    c.check_hostname = False
-    conn = c.wrap_socket(socket.socket(socket.AF_INET))
-    conn.connect((server, port))
-    return conn
 
-def from_taskrc():
-    conf = dict([x.replace("\\/", "/").strip().split('=') for x in open(
-        os.path.expanduser("~/.taskrc")).readlines() if '=' in x and x[0] != "#"])
-    #TODO parse out credentials
-    return connect(client_cert=conf['taskd.certificate'],
-        client_key=conf['taskd.key'],
-        server=conf['taskd.server'].split(":")[0],
-        port=int(conf['taskd.server'].split(":")[1]),
-        cacert=conf['taskd.ca'] if 'taskd.ca' in conf else None,
-        )
+class TaskdConnection(object):
+    def __init__(self):
+        self.port = 53589
+    
+    def from_taskrc(self):
+        conf = dict([x.replace("\\/", "/").strip().split('=') for x in open(
+            os.path.expanduser("~/.taskrc")).readlines() if '=' in x and x[0] != "#"])
+        self.client_cert=conf['taskd.certificate']
+        self.client_key=conf['taskd.key']
+        self.server=conf['taskd.server'].split(":")[0]
+        self.port=int(conf['taskd.server'].split(":")[1])
+        self.cacert=conf['taskd.ca'] if 'taskd.ca' in conf else None
+        self.group, self.username, self.uuid = conf['taskd.credentials'].split("/")
+    
+    def connect(self):
+        c = ssl.create_default_context()
+        c.load_cert_chain(client_cert, client_key)
+        if self.cacert:
+            c.load_verify_locations(cacert)
+        #enable for non-selfsigned certs
+        # print conn.getpeercert()
+        c.check_hostname = False
+        self.conn = c.wrap_socket(socket.socket(socket.AF_INET))
+        self.conn.connect((self.server, self.port))
+
+    def stats(self):
+        msg = transaction.mk_message(self.group, self.username, self.uuid)
+        msg['type'] = "statistics"
+        self.conn.sendall(transaction.prep_message(msg))
+        return self.recv()
+
+    def recv(self):
+        a = conn.recv(4096)
+        print struct.unpack('>L',a[:4])[0], "Byte Response"
+        resp = email.message_from_string(a[4:])
+
+        if 'code' in resp:
+            # print errors.Status(resp['code'])
+            if int(resp['code']) >= 400:
+                raise errors.Error(resp['code'])
+            if int(resp['code']) == 200:
+                print "Status Good!"
+        return resp
+
 def manual():
     # Task 2.3.0 doesn't let you have a cacert if you enable trust
     return connect(client_cert="/home/jack/.task/jacklaxson.cert.pem",
@@ -36,20 +59,8 @@ def manual():
         cacert="/home/jack/.task/ca.cert.pem",
         server="192.168.1.110",
         )
-# print conn.getpeercert()
-conn = manual()
-msg = transaction.mk_message("Public", "Jack Laxson", "f60bfcb9-b7b8-4466-b4c1-7276b8afe609")
-msg['type'] = "statistics"
-our_len = len(msg.as_string()) + 4
-size = struct.pack('>L', our_len)
-conn.sendall(size+msg.as_string())
 
-a = conn.recv(4096)
-print struct.unpack('>L',a[:4])[0], "Byte Response"
-
-resp = email.message_from_string(a[4:])
-if 'code' in resp:
-    # print errors.Status(resp['code'])
-    if int(resp['code']) >= 400:
-        raise errors.Error(resp['code'])
-print resp
+# from IPython import embed
+# embed()
+if __name__ == '__main__':
+    pass
--- taskc/transaction.py
+++ taskc/transaction.py
@@ -9,6 +9,13 @@ def mk_message(org, user, key):
     m['key'] = key
     return m
 
+def prep_message(msg):
+    """Add the size header"""
+    our_len = len(msg.as_string()) + 4
+    size = struct.pack('>L', our_len)
+    return size+msg.as_string()
+
+
 class Transaction(object):
     """ Each transaction is a single incoming message, with a single response
     message.  All communication therefore consists of a single 'send', followed
