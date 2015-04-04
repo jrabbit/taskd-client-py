38c5d14620780c9d22d61e629380787477f574dd
jrabbit <jackjrabbit+github@gmail.com>
2015-04-04 16:24:26 -0400

    test for stuff

--- taskc/simple_test.py
+++ taskc/simple_test.py
@@ -1,22 +1,36 @@
 import unittest
 from simple import TaskdConnection
+
+
 class TestConnection(unittest.TestCase):
+
     def setUp(self):
         self.tc = TaskdConnection()
+
     def test_rc(self):
         self.tc.from_taskrc("~/Projects/taskd-client-py/taskc/fixture/.taskrc")
 
-        self.assertEqual(self.tc.client_cert , "/home/jack/.task/jacklaxson.cert.pem")
-        self.assertEqual(self.tc.client_key , "/home/jack/.task/jacklaxson.key.pem")
-        self.assertEqual(self.tc.cacert , "/home/jack/.task/ca.cert.pem")
-        self.assertEqual(self.tc.server , "192.168.1.112")
-        self.assertEqual(self.tc.group , "Public")
-        self.assertEqual(self.tc.username , "Jack Laxson")
-        self.assertEqual(self.tc.uuid , "f60bfcb9-b7b8-4466-b4c1-7276b8afe609")
+        self.assertEqual(
+            self.tc.client_cert, "/home/jack/.task/jacklaxson.cert.pem")
+        self.assertEqual(
+            self.tc.client_key, "/home/jack/.task/jacklaxson.key.pem")
+        self.assertEqual(self.tc.cacert, "/home/jack/.task/ca.cert.pem")
+        self.assertEqual(self.tc.server, "192.168.1.112")
+        self.assertEqual(self.tc.port, 53589)
+        self.assertEqual(self.tc.group, "Public")
+        self.assertEqual(self.tc.username, "Jack Laxson")
+        self.assertEqual(self.tc.uuid, "f60bfcb9-b7b8-4466-b4c1-7276b8afe609")
+
     def test_connect(self):
         self.tc.from_taskrc("~/Projects/taskd-client-py/taskc/fixture/.taskrc")
 
         self.tc.connect()
+        self.assertEqual(self.tc.conn.getpeername(), ('192.168.1.112', 53589))
+        # make sure we're on TLS v2 per spec
+        self.assertEqual(self.tc.conn.context.protocol, 2)
+
+        # from IPython import embed
+        # embed()
 
 if __name__ == '__main__':
     unittest.main()
