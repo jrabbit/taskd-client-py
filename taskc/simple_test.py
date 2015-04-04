ddf98c8bee8ebce695d48a7d5789cc23196a29a9
jrabbit <jackjrabbit+github@gmail.com>
2015-04-04 15:48:24 -0400

    tests pass

--- taskc/simple_test.py
+++ taskc/simple_test.py
@@ -4,16 +4,16 @@ class TestConnection(unittest.TestCase):
     def setUp(self):
         self.tc = TaskdConnection()
     def test_rc(self):
-        self.tc.from_taskrc("fixture/.taskrc")
+        self.tc.from_taskrc("~/Projects/taskd-client-py/taskc/fixture/.taskrc")
 
         self.assertEqual(self.tc.client_cert , "/home/jack/.task/jacklaxson.cert.pem")
         self.assertEqual(self.tc.client_key , "/home/jack/.task/jacklaxson.key.pem")
-        self.assertEqual(self.tc.cacert , "/home/jack/.task/ca.cert.pem")
-        self.assertEqual(self.tc.server , "192.168.1.110")
+        self.assertEqual(self.tc.cacert , None)
+        self.assertEqual(self.tc.server , "192.168.1.112")
         self.assertEqual(self.tc.group , "Public")
         self.assertEqual(self.tc.username , "Jack Laxson")
         self.assertEqual(self.tc.uuid , "f60bfcb9-b7b8-4466-b4c1-7276b8afe609")
-
+    
 
 if __name__ == '__main__':
     unittest.main()
