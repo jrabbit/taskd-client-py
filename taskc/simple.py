a20f9df4773ba372843c3651e7baa3a2ed2356b9
Shir0kamii <shir0kamii@gmail.com>
2015-10-03 21:29:00 +0200

    Minimal refactoring of the code
    including :
     - autopep8
     - from_taskrc is now a classmethod
     - TaskdConnection.__init__ can now take a port as argument
     - more documentation
     - more pertinent variable names

--- taskc/simple.py
+++ taskc/simple.py
@@ -11,86 +11,148 @@ import errors
 
 class TaskdConnection(object):
 
-    def __init__(self):
-        self.port = 53589
+    def __init__(self, port=53589):
+        self.port = port
         self.cacert_file = False
         self.cacert = False
 
-    def from_taskrc(self, f="~/.taskrc"):
-        "Set all the required variables from a taskrc file"
+    @classmethod
+    def from_taskrc(cls, taskrc="~/.taskrc", **kwargs):
+        """
+        Build a TaskdConnection from a taskrc file
+
+        classmethod is used to allow inheritance
+
+        taskrc: configuration file for taskwarrior
+
+        kwargs: arguments to passe to the constructor of TaskdConnection
+        """
+
+        # Build TaskConnection object
+        connection = cls(**kwargs)
+
+        # Read the taskrc file to make a dict of attributes/values
         conf = dict([x.replace("\\/", "/").strip().split('=') for x in open(
-            os.path.expanduser(f)).readlines() if '=' in x and x[0] != "#"])
-        self.client_cert = conf['taskd.certificate']
-        self.client_key = conf['taskd.key']
-        self.server = conf['taskd.server'].split(":")[-2]
-        self.port = int(conf['taskd.server'].split(":")[-1])
-        self.cacert_file = conf['taskd.ca'] if 'taskd.ca' in conf else None
-        self.group, self.username, self.uuid = conf['taskd.credentials'].split("/")
-        return self
+            os.path.expanduser(taskrc)) if '=' in x and x[0] != "#"])
+
+        # Set instance variables according to taskrc file
+        connection.client_cert = conf['taskd.certificate']
+        connection.client_key = conf['taskd.key']
+        connection.server = conf['taskd.server'].split(":")[-2]
+        connection.port = int(conf['taskd.server'].split(":")[-1])
+        connection.cacert_file = conf[
+            'taskd.ca'] if 'taskd.ca' in conf else None
+        connection.group, connection.username, connection.uuid = conf[
+            'taskd.credentials'].split("/")
+
+        return connection
 
     def connect(self):
-        "Actually open the socket"
-        c = ssl.create_default_context()
-        c.load_cert_chain(self.client_cert, keyfile=self.client_key)
+        """
+        Actually open the socket
+        """
+
+        # Initialize an SSL context
+        context = ssl.create_default_context()
+        context.load_cert_chain(self.client_cert, keyfile=self.client_key)
         if self.cacert_file:
-            c.load_verify_locations(cafile=self.cacert_file)
+            context.load_verify_locations(cafile=self.cacert_file)
         elif self.cacert:
-            print self.cacert
-            c.load_verify_locations(cadata=self.cacert)
+            print self.cacert  # TODO: Replace prints with logging
+            context.load_verify_locations(cadata=self.cacert)
+
         # enable for non-selfsigned certs
         # print conn.getpeercert()
-        c.check_hostname = False
-        self.conn = c.wrap_socket(socket.socket(socket.AF_INET))
+        context.check_hostname = False
+
+        # create the socket
+        self.conn = context.wrap_socket(socket.socket(socket.AF_INET))
         self.conn.connect((self.server, self.port))
 
     def recv(self):
-        "Parse out the size header & read the message"
+        """
+        Parse out the size header & read the message
+        """
+
+        # receive data through the socket
         a = self.conn.recv(4)
-        bytez = struct.unpack('>L', a[:4])[0]
-        msg = self.conn.recv(bytez)
+
+        # Read the number of bytes of the message
+        # ">L" is 4 bytes long in big endian
+        bytes = struct.unpack('>L', a[:4])[0]
+
+        # Read the message itself
+        msg = self.conn.recv(bytes)
+
+        # logging data
         logging.info("%s Byte Response", bytez)
         logging.debug(msg)
+
+        # parse the response
         resp = email.message_from_string(
             msg, _class=transaction.TaskdResponse)
 
+        # TODO: This part should use logging
         if 'code' in resp:
             # print errors.Status(resp['code'])
             if int(resp['code']) >= 400:
                 print resp['code']
             if int(resp['code']) == 200:
                 print "Status Good!"
+
         return resp
 
     def _mkmsg(self, ttype):
+        """
+        Make message
+        """
+
         msg = transaction.mk_message(self.group, self.username, self.uuid)
         msg['type'] = ttype
+
         return transaction.prep_message(msg)
 
     def stats(self):
-        """Get some statistics from the server"""
+        """
+        Get some statistics from the server
+        """
+
         self.conn.sendall(self._mkmsg("statistics"))
+
         return self.recv()
 
     def pull(self):
-        """Get all the tasks down from the server"""
+        """
+        Get all the tasks down from the server
+        """
+
         self.conn.sendall(self._mkmsg("sync"))
+
         return self.recv()
 
     def put(self, tasks):
-        """Push all our tasks to server
-           tasks - taskjson list
         """
+        Push all our tasks to server
+
+        tasks: taskjson list
+        """
+
         msg = transaction.mk_message(self.group, self.username, self.uuid)
         msg.set_payload(tasks)
         msg['type'] = 'sync'
         tx_msg = transaction.prep_message(msg)
         self.conn.sendall(tx_msg)
+
         return self.recv()
 
     def sync(self, sync_key):
-        """Sync our tasks and server's, takes sync_key (uuid debounce from previous txn)"""
+        """
+        Sync our tasks and server's, takes sync_key (uuid debounce from previous txn)
+        """
         pass
 
+
+# Should be deprecated soon
 def manual():
     # Task 2.3.0 doesn't let you have a cacert if you enable trust
     tc = TaskdConnection()
