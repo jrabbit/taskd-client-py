10676cf87978562335ea4de839938dba6bb06272
jrabbit <jackjrabbit@gmail.com>
2015-10-04 20:41:54 -0400

    docker scripts to begin work on #12

---
+++ post_test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker rm -f taskc_test
---
+++ pre_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run -it -p 53589:53589 -d --name taskc_test jrabbit/taskd
+export TEST_UUID=`docker exec taskc_test taskd add user Public test_user | grep key | awk '{ print substr($0, 15)}'`
