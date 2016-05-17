1dc6f1d322b7c50e9ea3618fd211fda2c06045d4
jrabbit <jackjrabbit@gmail.com>
2016-05-17 15:36:29 -0400

    docker testing almost

--- taskc/simple_test.py
+++ taskc/simple_test.py
@@ -1,5 +1,8 @@
 import unittest
 import os
+
+from docker import Client
+
 from simple import TaskdConnection
 
 
@@ -24,10 +27,19 @@ class TestRCParse(unittest.TestCase):
 class TestConnection(unittest.TestCase):
 
     def setUp(self):
+        self.docker = Client(base_url='unix://var/run/docker.sock')
+        host_config = self.docker.create_host_config(port_bindings={53589:9001,},publish_all_ports=True)
+        self.container = self.docker.create_container("jrabbit/taskd", name="taskc_test", host_config=host_config)
+        self.docker.start(self.container["Id"])
+        our_exec = self.docker.exec_create(self.container["Id"], "taskd add user Public test_user")
         self.tc = TaskdConnection()
+        o = self.docker.exec_start(our_exec['Id'])
+        print o
+        self.tc.uuid = o.split('\n')[0].split()[-1]
+        print self.tc.uuid
         self.tc.server = "localhost"
         self.tc.port = 9001
-        self.tc.uuid = os.getenv("TEST_UUID")
+        # self.tc.uuid = os.getenv("TEST_UUID")
         self.tc.group = "Public"
         self.tc.username = "test_user"
         self.tc.client_cert = "taskc/fixture/pki/client.cert.pem"
@@ -36,7 +48,7 @@ class TestConnection(unittest.TestCase):
 
     def test_connect(self):
 
-        self.tc.connect()
+        self.tc._connect()
         # print self.tc.conn.getpeername()
         self.assertEqual(self.tc.conn.getpeername(), ('127.0.0.1', 9001))
         # make sure we're on TLS v2 per spec
@@ -46,17 +58,17 @@ class TestConnection(unittest.TestCase):
         # embed()
 
     def test_put(self):
-
-        self.tc.connect()
+        assert self.tc.uuid
         self.tc.put("")
-        self.tc.connect()
         tasks = """{"description":"hang up posters","entry":"20141130T081652Z","status":"pending","uuid":"0037aa92-45e5-44a6-8f34-2f92989f173a"}
 {"description":"make pb ramen","entry":"20141130T081700Z","status":"pending","uuid":"dd9b71db-f51c-4026-9e46-bb099df8dd3f"}
 {"description":"fold clothes","entry":"20141130T081709Z","status":"pending","uuid":"d0f53865-2f01-42a8-9f9e-3652c63f216d"}"""
         resp = self.tc.put(tasks)
         self.assertEqual(resp.status_code, 200)
         # might not be correct depends on state of taskd
-
+    def tearDown(self):
+        # self.docker.remove_container(self.container['Id'], force=True)
+        pass
 # class TestStringIO(unittest.TestCase):
 #     def setUp(self):
 #         self.tc = TaskdConnection()
